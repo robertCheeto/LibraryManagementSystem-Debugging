1fee9b665849a84df958f435481a2ab618488832
Daniel Spezialetti <dspezialetti@my.yearupunited.org>
2025-10-29 15:12:13 -0400

    * began working on bonus ticket - 25 (immutable value objects)

---
+++ src/main/java/com/pluralsight/model/ItemDetails.java
@@ -0,0 +1,8 @@
+package com.pluralsight.model;
+
+public class ItemDetails extends Item {
+    public ItemDetails(String id, String title, String creator, String genre) {
+        super(id, title, creator, genre);
+    }
+
+}
